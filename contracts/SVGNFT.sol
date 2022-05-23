cb61f2ad7d772b6872f83b1408ba8ee3fe3eebf8
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-05-23 19:12:25 +0530

    Added constructor

--- contracts/SVGNFT.sol
+++ contracts/SVGNFT.sol
@@ -1,6 +1,15 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-contract SVGNFT {
-    uint256 public name;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "base64-sol/base64.sol";
+
+contract SVGNFT is ERC721URIStorage, Ownable {
+    uint256 public tokenCounter;
+    event CreatedSVGNFT(uint256 indexed tokenId, string tokenURI);
+
+    constructor() ERC721("SVG NFT", "svgNFT") {
+        tokenCounter = 0;
+    }
 }
